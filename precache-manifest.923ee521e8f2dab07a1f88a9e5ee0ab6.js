9780c181d1da7c2bc94f97607a1b373f576388b0
Manish Khedekar <mankhedekar96@gmail.com>
2020-07-11 16:46:06 +0530

    Updates

---
+++ precache-manifest.923ee521e8f2dab07a1f88a9e5ee0ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e89d9da1329dafbd3737a220c4dfa",
+    "url": "/xpertdrivingacademy/index.html"
+  },
+  {
+    "revision": "836e02939f0789d62a2b",
+    "url": "/xpertdrivingacademy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98f02376ff3c327d8ee1",
+    "url": "/xpertdrivingacademy/static/js/2.0a3cfed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xpertdrivingacademy/static/js/2.0a3cfed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836e02939f0789d62a2b",
+    "url": "/xpertdrivingacademy/static/js/main.71b47a27.chunk.js"
+  },
+  {
+    "revision": "0bf6e25e5b890ef7a684",
+    "url": "/xpertdrivingacademy/static/js/runtime-main.0ff871b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xpertdrivingacademy/static/media/logo.5d5d9eef.svg"
+  }
+]);
